086aee4eca7cf66fd56ab336f587ffd3fb0c5314
jvb93 <1976577+jvb93@users.noreply.github.com>
2021-11-10 09:41:14 -0800

    add producer

---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = BuildServiceProvider();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var redis = scope.ServiceProvider.GetRequiredService<ConnectionMultiplexer>();
+                var redisOptions = scope.ServiceProvider.GetRequiredService<RedisOptions>();
+                var db = redis.GetDatabase();
+
+                while (true)
+                {
+                    Console.WriteLine("Enter a message:");
+                    var streamValue = Console.ReadLine();
+                    var id = await db.StreamAddAsync(redisOptions.StreamName, "Message", streamValue);
+                    Console.WriteLine($"Wrote id {id} to stream {redisOptions.StreamName}");
+                }
+            }
+        }
+
+        static IServiceProvider BuildServiceProvider()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            var serviceCollection = new ServiceCollection();
+            var localOptions = new RedisOptions();
+            config.GetSection("Redis").Bind(localOptions);
+
+            serviceCollection.AddSingleton(localOptions);
+            serviceCollection.AddSingleton(ConnectionMultiplexer.Connect($"{localOptions.Host}:{localOptions.Port}"));
+            
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
---
+++ Producer/Producer/RedisOptions.cs
@@ -0,0 +1,6 @@
+public class RedisOptions
+{
+    public string Host { get; set; }
+    public ushort Port { get; set; }
+    public string StreamName { get; set; }
+}
